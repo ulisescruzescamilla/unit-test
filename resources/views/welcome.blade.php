0cf0b4ace86bd97fc3d45a118cc3cce111446475
Ulises Escamilla <ulisescruzescamilla@gmail.com>
2023-01-11 23:23:32 -0600

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,20 @@
 
     </head>
     <body class="bg-gray">
-        @foreach ($repositories as $repo)
-            <h2>{{$repo->url}}</h2>
-            <p>{{$repo->description}}</p>
-        @endforeach
+        <ul class="max-w-lg bg-white border-r border-gray-300 shadow-xl">
+            @foreach ($repositories as $repo)
+                <li class="flex items-center text-black p-2 hover:bg-ray-300">
+                    <img class="{{$repository->user->profile_photo_url}}" class="w-12 h-12 rounded-full mr-2">
+                </li>
+
+                <div class="flex justify-bewteen w-full">
+                    <div class="flex-1">
+                        <h2 class="text-sm font-semibold text-black">{{$repo->url}}</h2>
+                        <p>{{$repo->description}}</p>
+                    </div>
+                    <span class="text-xs font-medium text-gray-600">{{$repo->created_at}}</span>
+                </div>
+            @endforeach
+        </ul>
     </body>
 </html>
